7f8c365f87be5045d8bf40a877484b3f1a38d2dc
ShinjungOh <tlswjd9260@gmail.com>
2026-01-13 03:43:30 +0900

    feat: 헤더의 로고 클릭 시 새로고침

--- apps/frontend/src/components/layout/Header.tsx
+++ apps/frontend/src/components/layout/Header.tsx
@@ -1,13 +1,19 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 export const Header = () => {
+  const handleLogoClick = () => {
+    window.location.reload();
+  };
+
   return (
     <Box as="header" h="60px" display="flex" alignItems="center" px={6} borderBottom="1px solid black">
-      <img
-        src="https://cdn.datarize.io/console-v4/imgs/logo-black.svg"
-        alt="logo"
-        style={{ objectFit: 'contain', width: 140 }}
-      />
+      <Flex h="100%" onClick={handleLogoClick}>
+        <img
+          src="https://cdn.datarize.io/console-v4/imgs/logo-black.svg"
+          alt="logo"
+          style={{ objectFit: 'contain', width: 140, cursor: 'pointer' }}
+        />
+      </Flex>
     </Box>
   );
 };
